5fd9df6c5a87d2903e8457b0e2afae7d0948569a
harshilpec17 <harshilsuthar@hotmail.com>
2023-05-15 14:24:04 -0400

    Search bar added

--- 04 Food Ordering App/Assignment 05.md
+++ 04 Food Ordering App/Assignment 05.md
@@ -40,3 +40,20 @@ export function fullName() {
 // ModuleB
 import * as ModuleA from './ModuleA';
 ```
+
+## What is the importance of `config.js` file ?
+
+- It is very important file for centralize data, like API keys, environment-specific variables, features and other customizable option, The developer can easily modify and update the file without making code change. encryption keys and password and token can be store at one place, we can use that without exposing the data security. In the team work, it is very specific file where all team member will have the same configuration.
+
+## what are React Hooks ?
+
+- React hooks are features update after 16.8 update. It gives you a function components, and do not require to use the classes in the react hooks, before this updates, logic were encapsulated within the class components like `componentDidMount`, `componentDidUpdate` and `componentWillUnmount`. It is very complex method.
+- `useState`: allows function components to have local state variable and function to update variable
+- `useEffect`: It replace the lifecycle method like `componentDidMount`, `componentDidUpdate` and `componentWillUnmount`.
+
+- There are plenty of other hooks available to work on
+
+## Why do we need useState Hooks ?
+
+- it provide the way to store and update the state within components. enabling to manage dynamic data and trigger re-render when the state change
+- each call to `useState` creates a separate state variable and associate to setter function. This flexibility makes easy to use the same variable at different place by using setter function.
--- 04 Food Ordering App/src/Component/Body.jsx
+++ 04 Food Ordering App/src/Component/Body.jsx
@@ -4,29 +4,56 @@ import { useState } from "react";
 
 const Body = () => {
   const [newList, setNewList] = useState(restObj);
+  const [typedText, setTypedText] = useState("");
   return (
     <>
-      <div className="filter-btn">
-        <button
-          className="btn"
-          onClick={() => {
-            const topRated = newList.filter((x) => x.info.avgRating > 4);
-            setNewList(topRated);
-          }}
-        >
-          Top Rated Restaurants
-        </button>
-        <button
-          className="btn"
-          onClick={() => {
-            const fastDelivery = newList.filter(
-              (x) => x.info.sla.deliveryTime < 28 
-            );
-            setNewList(fastDelivery);
-          }}
-        >
-          Faster Delivery
-        </button>
+      <div className="wrapper">
+        <div className="filter-btns">
+          <button
+            className="btn"
+            onClick={() => {
+              const topRated = newList.filter((x) => x.info.avgRating > 4);
+              setNewList(topRated);
+            }}
+          >
+            Top Rated Restaurants
+          </button>
+          <button
+            className="btn fasterDelivery"
+            onClick={() => {
+              const fastDelivery = newList.filter(
+                (x) => x.info.sla.deliveryTime < 28
+              );
+              setNewList(fastDelivery);
+            }}
+          >
+            Faster Delivery
+          </button>
+        </div>
+        <div className="searchBar">
+          <input
+            type="search"
+            id="searchBar"
+            placeholder="Find a Spot"
+            value={typedText}
+            onChange={(text) => setTypedText(text.target.value)}
+          />
+          <button
+            className="btn"
+            onClick={() => {
+              const search = newList.filter((text) => { for(i = 0; i < text.info.name.length; i++){
+                if(text.info.name[i].toLowerCase == typedText.toLowerCase)
+                return text;
+              }
+              
+              });
+              setNewList(search);
+              console.log(search);
+            }}
+          >
+            Search
+          </button>
+        </div>
       </div>
       <div className="container">
         {newList.map((rest) => (
